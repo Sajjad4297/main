37e00522171a4e2ee0ff4ec8519a4311aa6bc556
sajjad4297 <sajjad.ranjbaran.4297@gmail.com>
2024-08-15 10:32:15 +0330

    asdad

--- cookie.js
+++ cookie.js
@@ -1,8 +1,8 @@
 function SetCookie(name, value, age) {
-   // var d = new Date();
-   // d.setTime(d.getTime() + (exday * 24 * 60 * 60 * 1000));
-   // var expires = "expires=" + d.toGMTString();
-    document.cookie = name + "=" + value + ";" +"max-age=" + age + ";path=/";
+    // var d = new Date();
+    // d.setTime(d.getTime() + (exday * 24 * 60 * 60 * 1000));
+    // var expires = "expires=" + d.toGMTString();
+    document.cookie = name + "=" + value + ";" + "max-age=" + age + ";path=/";
 }
 
 function getCookie(name) {
@@ -25,47 +25,51 @@ function getCookie(name) {
 
 function CheckCookie() {
     var user = getCookie("username");
-    if (user == "Sajy") {
+    if (user == "Sajy1384") {
     }
-    else if(user=="BUG"){
-        newWindow();
-    }
-    else if (user=="") {
-        user=prompt("Please Enter password","");
-            if (user=="BUG") {
-                SetCookie('username',"BUG",300);
-                window.location.reload();
-            }
-            else if(user==""){
-                SetCookie('username',"block",300);
-                window.location.reload();
+    // else if (user == "BUG") {
+    //     newWindow();
+    // }
+    else if (user == "") {
+        user = prompt("Please Enter password", "");
+        if (user == "BUG") {
+            SetCookie('username', "BUG", 300);
+            window.location.reload();
+        }
+        else if (user == "") {
+            SetCookie('username', "block", 300);
+            window.location.reload();
 
-            }
-            else{
-                SetCookie('username',"block",300);
-                window.location.reload();
-            }
+        }
+        else {
+            SetCookie('username', "block", 300);
+            window.location.reload();
+        }
     }
-    else{
-
-        document.body.style.display="none";
+    else {
+        newWindow()
     }
 }
 function newWindow() {
     var array = [];
-    for(i=0;i<=60;i++){
-       if (i<15) {
-        array[i] = window.open("","","width=50,height=50,left="+(i*100)+",top=0" );
-       }
-       else if(i>=15 && i<30){
-        array[i] = window.open("","","width=50,height=50,left="+((i-15)*100)+",top=150" );
-       }
-       else if(i>=30 && i<45){
-        array[i] = window.open("","","width=50,height=50,left="+((i-30)*100)+",top=300" );
-       }
-       else if(i>=45 && i<60){
-        array[i] = window.open("","","width=50,height=50,left="+((i-45)*100)+",top=450" );
-       }
-       array[i].document.write("BUG...");
+    for (i = 0; i <= 90; i++) {
+        if (i < 15) {
+            array[i] = window.open("", "", "width=50,height=50,left=" + (i * 100) + ",top=0");
+        }
+        else if (i >= 15 && i < 30) {
+            array[i] = window.open("", "", "width=50,height=50,left=" + ((i - 15) * 100) + ",top=150");
+        }
+        else if (i >= 30 && i < 45) {
+            array[i] = window.open("", "", "width=50,height=50,left=" + ((i - 30) * 100) + ",top=300");
+        }
+        else if (i >= 45 && i < 60) {
+            array[i] = window.open("", "", "width=50,height=50,left=" + ((i - 45) * 100) + ",top=450");
+        } else if (i >= 60 && i < 75) {
+            array[i] = window.open("", "", "width=50,height=50,left=" + ((i - 60) * 100) + ",top=600");
+        }
+        else if (i >= 75 && i < 90) {
+            array[i] = window.open("", "", "width=50,height=50,left=" + ((i - 75) * 100) + ",top=750");
+        }
+        array[i].document.write("BUG...");
     }
 }
