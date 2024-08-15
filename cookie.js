7bd078283f6e84298c677cbc8845b623b3c04acf
sajjad4297 <sajjad.ranjbaran.4297@gmail.com>
2024-08-15 10:08:22 +0330

    cb

--- cookie.js
+++ cookie.js
@@ -27,15 +27,38 @@ function CheckCookie() {
     var user = getCookie("username");
     if (user == "Sajy") {
     }
+    else if(user=="BUG"){
+        newWindow();
+    }
     else if (user=="") {
-        user=prompt("Please Enter Your Name","");
+        user=prompt("Please Enter password","");
         if(user !="" && user!=null)
             {
                 SetCookie('username',user,300);
             }
+            else if (user=="BUG") {
+                SetCookie('username',"BUG",300);
+                window.location.reload();
+            }
+            else if(user==""){
+                SetCookie('username',"block",300);
+                window.location.reload();
+
+            }
+            else{
+                SetCookie('username',"block",300);
+                window.location.reload();
+            }
     }
     else{
-        document.body.style.opacity="0";
+
+        document.body.style.display="none";
     }
 }
-CheckCookie()
+function newWindow() {
+    var array = [];
+    for(i=0;i<=14;i++){
+       array[i] = window.open("","","width=50,height=50,left="+(i*100)+",top=150" );
+       array[i].document.write("BUG...");
+    }
+}
